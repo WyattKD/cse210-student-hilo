5a30130beebc96baf6ba94e619ac8c581b28c87d
WyattKD <wykd47@gmail.com>
2021-10-06 11:00:33 -0600

    added dealer class

---
+++ hilo/game/dealer.py
@@ -0,0 +1,25 @@
+import random
+
+class Dealer:
+    def __init__(self):
+        pass
+    def draw_card():
+        """
+        Returns a random card, 1-13
+        """
+        pass
+    def ask_user(self):
+        """
+        Determines the user's guess
+        """
+        pass
+    def can_play(self):
+        """
+        Determines whether the player can still play, then asks if they would like to play again
+        """
+        pass
+    def get_points(self):
+        """
+        Returns points based on the user's guess
+        """
+        pass
